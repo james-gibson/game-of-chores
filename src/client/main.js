49457eb7e1926336f776ffab1a2e1016e1ef9029
James Gibson <james@apto.com>
2016-08-06 14:34:28 -0600

    initial file to load

---
+++ src/client/main.js
@@ -0,0 +1,7 @@
+/* This File is the main entry point for the application, all dependencies should be declared here*/
+
+// This instantiates the application logic
+require('./app/main.js');
+
+// This imports any styles needed
+require('./styles/styles.scss');
